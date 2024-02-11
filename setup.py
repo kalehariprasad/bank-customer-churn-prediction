de516b20b639afea23d10ee4276dd0d0626a52ad
hari prasad <hariprasad9693@gmail.com>
2024-02-11 12:40:15 +0530

    intial commit with cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project will help to make desions about the customers who are likely to churn',
+    author='hari prasad',
+    license='',
+)
